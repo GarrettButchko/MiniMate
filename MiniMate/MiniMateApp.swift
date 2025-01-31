2c303c9406b56b4a5c34852074bd1d6e4d4670ac
GarrettButchko <garrettwm2005@gmail.com>
2025-01-31 15:01:24 -0500

    Initial Commit

---
+++ MiniMate/MiniMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMateApp.swift
+//  MiniMate
+//
+//  Created by Garrett Butchko on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
